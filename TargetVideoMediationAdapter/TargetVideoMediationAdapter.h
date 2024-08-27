6caf0386f929e6a000db73a2d667cbbb654db9bc
Predrag Jevtic <predragjevtic@gmail.com>
2024-08-27 10:20:25 +0200

    Initial Commit

---
+++ TargetVideoMediationAdapter/TargetVideoMediationAdapter.h
@@ -0,0 +1,12 @@
+//
+//  TargetVideoMediationAdapter.h
+//  TargetVideoMediationAdapter
+//
+//  Created by Predrag Jevtic on 27.8.24..
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TargetVideoMediationAdapter : NSObject
+
+@end
---
+++ TargetVideoMediationAdapter/TargetVideoMediationAdapter.m
@@ -0,0 +1,12 @@
+//
+//  TargetVideoMediationAdapter.m
+//  TargetVideoMediationAdapter
+//
+//  Created by Predrag Jevtic on 27.8.24..
+//
+
+#import "TargetVideoMediationAdapter.h"
+
+@implementation TargetVideoMediationAdapter
+
+@end
